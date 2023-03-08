624e1e142f15ea38d5cd914c372fd204edf437f7
dhthanhqa@gmail.com <dhthanhqa@gmail.com>
2023-03-08 07:13:08 +0700

    update data

--- saleappv1/saleapp/index.py
+++ saleappv1/saleapp/index.py
@@ -8,57 +8,6 @@ def index():
     category_id = request.args.get('category_id')
     products = dao.get_products(kw=kw, category_id=category_id)
 
-    # products = [{
-    #     "id": 1,
-    #     "name": "iPhone 7 Plus",
-    #     "description": "Apple, 32GB, RAM: 3GB, iOS13",
-    #     "price": 17000000,
-    #     "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647056401/ipmsmnxjydrhpo21xrd8.jpg",
-    #     "category_id": 1
-    # }, {
-    #     "id": 2,
-    #     "name": "iPad Pro 2020",
-    #     "description": "Apple, 128GB, RAM: 6GB",
-    #     "price": 37000000,
-    #     "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1646729533/zuur9gzztcekmyfenkfr.jpg",
-    #     "category_id": 2
-    # }, {
-    #     "id": 3,
-    #     "name": "Galaxy Note 10 Plus",
-    #     "description": "Samsung, 64GB, RAML: 6GB",
-    #     "price": 24000000,
-    #     "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647248722/r8sjly3st7estapvj19u.jpg",
-    #     "category_id": 1
-    # }, {
-    #     "id": 4,
-    #     "name": "Galaxy S23",
-    #     "description": "Samsung, 64GB, RAML: 6GB",
-    #     "price": 24000000,
-    #     "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647248722/r8sjly3st7estapvj19u.jpg",
-    #     "category_id": 1
-    # }, {
-    #     "id": 5,
-    #     "name": "Galaxy S22",
-    #     "description": "Samsung, 64GB, RAML: 6GB",
-    #     "price": 24000000,
-    #     "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647248722/r8sjly3st7estapvj19u.jpg",
-    #     "category_id": 1
-    # }, {
-    #     "id": 6,
-    #     "name": "Galaxy S22",
-    #     "description": "Samsung, 64GB, RAML: 6GB",
-    #     "price": 24000000,
-    #     "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647248722/r8sjly3st7estapvj19u.jpg",
-    #     "category_id": 1
-    # }, {
-    #     "id": 7,
-    #     "name": "Galaxy S22",
-    #     "description": "Samsung, 64GB, RAML: 6GB",
-    #     "price": 24000000,
-    #     "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647248722/r8sjly3st7estapvj19u.jpg",
-    #     "category_id": 1
-    # }]
-
     return render_template("index.html", products=products)
 
 
--- saleappv1/saleapp/models.py
+++ saleappv1/saleapp/models.py
@@ -20,9 +20,67 @@ class Product(db.Model):
 
 if __name__ == '__main__':
     with app.app_context():
-        # c1 = Category(name='Điện thoại di động')
-        # c2 = Category(name='Máy tính bảng')
-        # db.session.add_all([c1, c2])
-        # db.session.commit()
-        # db.drop_all()
         db.create_all()
+
+        c1 = Category(name='Điện thoại di động')
+        c2 = Category(name='Máy tính bảng')
+        db.session.add_all([c1, c2])
+        db.session.commit()
+
+        products = [{
+            "id": 1,
+            "name": "iPhone 7 Plus",
+            "description": "Apple, 32GB, RAM: 3GB, iOS13",
+            "price": 17000000,
+            "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647056401/ipmsmnxjydrhpo21xrd8.jpg",
+            "category_id": 1
+        }, {
+            "id": 2,
+            "name": "iPad Pro 2020",
+            "description": "Apple, 128GB, RAM: 6GB",
+            "price": 37000000,
+            "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1646729533/zuur9gzztcekmyfenkfr.jpg",
+            "category_id": 2
+        }, {
+            "id": 3,
+            "name": "Galaxy Note 10 Plus",
+            "description": "Samsung, 64GB, RAML: 6GB",
+            "price": 24000000,
+            "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647248722/r8sjly3st7estapvj19u.jpg",
+            "category_id": 1
+        }, {
+            "id": 4,
+            "name": "Galaxy S23",
+            "description": "Samsung, 64GB, RAML: 6GB",
+            "price": 24000000,
+            "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647248722/r8sjly3st7estapvj19u.jpg",
+            "category_id": 1
+        }, {
+            "id": 5,
+            "name": "Galaxy S22",
+            "description": "Samsung, 64GB, RAML: 6GB",
+            "price": 24000000,
+            "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647248722/r8sjly3st7estapvj19u.jpg",
+            "category_id": 1
+        }, {
+            "id": 6,
+            "name": "Galaxy S22",
+            "description": "Samsung, 64GB, RAML: 6GB",
+            "price": 24000000,
+            "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647248722/r8sjly3st7estapvj19u.jpg",
+            "category_id": 1
+        }, {
+            "id": 7,
+            "name": "Galaxy S22",
+            "description": "Samsung, 64GB, RAML: 6GB",
+            "price": 24000000,
+            "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647248722/r8sjly3st7estapvj19u.jpg",
+            "category_id": 1
+        }]
+
+        for p in products:
+            del p['id']
+            pro = Product(**p)
+            db.session.add(pro)
+
+        db.session.commit()
